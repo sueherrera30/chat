df54853f18ee0f82fa1f85cc1cf5ba0a23975166
Sue Martinez Herrera <sue.martinez@MacBook-Pro-de-Sue.local>
2023-03-07 18:49:52 -0600

    feat: add config for less

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello chat!</p>
+    </div>
+  );
+}
+
+export default App;
